724c48b4ea25b88aae033c3ea16f254e36d7dfdb
Dayane Pabla <dayanepabla@dayanepabla.(none)>
2014-03-25 11:15:21 -0300

    Criação das views para diary e contacts

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/diary')
+def diary():
+    return render_template("diary.html")
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
 if __name__ == '__main__':
     app.run()
