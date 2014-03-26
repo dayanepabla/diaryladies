97f4eb8184f5679d6828545ce219bf1e955d4acc
Dayane Pabla <dayanepabla@gmail.com>
2014-03-25 23:24:48 -0300

    Adição do formulário para contatos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,5 +13,13 @@ def diary():
 def contacts():
     return render_template("contacts.html")
 
+@app.route('/contacts/add')
+def contacts_add():
+    return render_template("contacts_add.html")
+
+@app.route('/contacts/add', methods=['POST'])
+def contacts_add_post():
+      return "Contato salvo com sucesso!"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
