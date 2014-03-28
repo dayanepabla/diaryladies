88b61ad0ca633bbd515d6128546d3298cc09026d
Dayane Pabla <dayanepabla@gmail.com>
2014-03-27 22:01:54 -0300

    Adição das funções editar e remover

--- app.py
+++ app.py
@@ -10,11 +10,28 @@ def index():
 
 @app.route('/diary')
 def diary():
-    return render_template("diary.html")
+    diary = mongo.db.diary.find()
+    return render_template("diary.html", diary=diary)
+
+@app.route('/diary/add')
+def diary_add():
+    return render_template("diary_add.html")
+
+@app.route('/diary/add', methods=['POST'])
+def diary_add_post():
+    nota = {"tarefa":request.form["tarefa"], "data":request.form["data"]}
+    mongo.db.diary.insert(nota)
+    return "Tarefa adicionada com sucesso!"
+
+@app.route('/diary/<ObjectId:id>/remover')
+def tarefa_rmv(id):
+    mongo.db.diary.remove(id)
+    return "Removido com sucesso"
 
 @app.route('/contacts')
 def contacts():
-    return render_template("contacts.html")
+    contacts = mongo.db.contacts.find()
+    return render_template("contacts.html", contacts=contacts)
 
 @app.route('/contacts/add')
 def contacts_add():
@@ -25,6 +42,11 @@ def contacts_add_post():
     data = {"nome":request.form["nome"], "email":request.form["email"], "telefone":request.form["telefone"]}
     mongo.db.contacts.insert(data)
     return "Contato salvo com sucesso!"
+
+@app.route('/contacts/<ObjectId:id>/remover')
+def contacts_rmv(id):
+    mongo.db.contacts.remove(id)
+    return "Removido com sucesso"
 
 if __name__ == '__main__':
     app.run(debug=True)
